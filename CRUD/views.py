d28edba9bc9e0590c531f09dc91eebe1c49c4323
Sodiq <murodovsodiq1800@gmail.com>
2022-07-10 12:21:52 +0500

    sodiq_new

--- CRUD/views.py
+++ CRUD/views.py
@@ -8,17 +8,16 @@ def myfunc(request):
     surname = ['Murodov', 'Xakimov', 'Yusupov', 'Shoyev', 'Hikmatov', 'Aliyev']
     desc = ['asdfsdfsdf', 'asdfsff', 'fghththfgh', 'trg65hrther', 'grg56yjghnghfng', 'ert54gryfbsf']
     for x in range(100):
-        base = Main.objects.create(
+        Main.objects.create(
             first_name=random.choice(name),
             last_name=random.choice(surname),
             age=random.randint(1, 100),
             desc=random.choice(desc)
         )
     data = Main.objects.all()
-    return render(request, 'index.html', {'base': base, 'data': data})
+    return render(request, 'index.html', {'data': data})
 
 
 def rm(request):
     Main.objects.all().delete()
     return render(request, 'index.html')
-
